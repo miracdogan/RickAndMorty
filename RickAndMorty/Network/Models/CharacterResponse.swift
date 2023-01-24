ca52a3e6b521c7a11c4a947b78efb1625d235e43
Miraç Doğan <miracdogn@gmail.com>
2023-01-24 22:56:41 +0300

    Species and status added for each character

--- RickAndMorty/Network/Models/CharacterResponse.swift
+++ RickAndMorty/Network/Models/CharacterResponse.swift
@@ -17,5 +17,7 @@ struct CharacterResponse: Decodable {
 struct Characters: Decodable {
     let id: Int?
     let name: String?
+    let status: String?
+    let species: String?
     let image: URL?
 }
--- RickAndMorty/Pages/Characters/Cell/CharacterTableViewCell.swift
+++ RickAndMorty/Pages/Characters/Cell/CharacterTableViewCell.swift
@@ -12,10 +12,15 @@ class CharacterTableViewCell: UITableViewCell {
 
     @IBOutlet weak var characterImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var speciesLabel: UILabel!
+    
     
     func configure(with character: Characters) {
         
         nameLabel.text = character.name
+        statusLabel.text = character.status
+        speciesLabel.text = character.species
         characterImageView.setImage(with: character.image!)
     }
 }
